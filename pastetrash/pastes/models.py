33f02d411a61b44e9975ad7d0ba35006377a1035
Roméo <romeo@phang.fr>
2023-08-20 20:20:01 +0200

    feat: added Paste logic & tests

--- pastetrash/pastes/models.py
+++ pastetrash/pastes/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils.text import slugify
 # Create your models here.
 class Paste(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, editable=False)
     syntax = models.CharField(max_length=100)
     password = models.CharField(max_length=100, null=True, blank=True)
+    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
+    
+    def check_password(self, password):
+        return check_password(password, self.password)
+
+    def save(self, *args, **kwargs):
+        # Empty values
+        if not self.title:
+            raise ValueError("Title is empty")
+
+        if not self.content:
+            raise ValueError("Content is empty")
+
+        # Slug
+        self.slug = slugify(self.title)
+        # Password
+        if self.password:
+            self.password = make_password(self.password)
+        super(Paste, self).save(*args, **kwargs)
--- pastetrash/pastes/tests.py
+++ pastetrash/pastes/tests.py
@@ -1,3 +1,52 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
+from .models import Paste
 # Create your tests here.
+
+class PasteTestCase(TestCase):
+    def setUp(self):
+        Paste.objects.create(title="test", content="test content", password="test")
+
+    def test_paste_read(self):
+        paste = Paste.objects.get(title="test")
+        self.assertEqual(paste.content, "test content")
+        self.assertEqual(paste.slug, "test")
+    
+    def test_paste_password_is_valid(self):
+        paste = Paste.objects.get(title="test")
+        self.assertNotEqual(paste.password, "test")
+        self.assertTrue(paste.check_password("test"))
+
+    def test_paste_create(self):
+        paste = Paste.objects.create(title="teste2", content="test content2", password="teste2")
+        self.assertEqual(paste.content, "test content2")
+        self.assertEqual(paste.slug, "teste2")
+        self.assertNotEqual(paste.password, "teste2")
+
+    def test_paste_delete(self):
+        paste = Paste.objects.get(title="test")
+        paste.delete()
+        self.assertEqual(Paste.objects.all().count(), 0)
+
+    def test_paste_update(self):
+        paste = Paste.objects.get(title="test")
+        paste.title = "test2"
+        paste.content = "test content2"
+        paste.syntax = "python"
+        paste.password = "test2"
+        paste.save()
+        self.assertEqual(paste.content, "test content2")
+        self.assertEqual(paste.slug, "test2")
+        self.assertEqual(paste.syntax, "python")
+        self.assertNotEqual(paste.password, "test2")
+        self.assertEqual(Paste.objects.all().count(), 1)
+
+    def test_fail_slug(self):
+        self.assertRaises(IntegrityError, Paste.objects.create, **{"title":"test", "content":"test content"})
+
+    def test_fail_empty_title(self):
+        self.assertRaises(ValueError, Paste.objects.create, **{"title":"", "content":"test content"})
+
+    def test_fail_empty_content(self):
+        self.assertRaises(ValueError, Paste.objects.create, **{"title":"test", "content":""})
